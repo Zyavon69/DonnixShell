c8ba8099e2e02407994ce1683a07ef86dd29af64
Zyavon69 <sbilous045@gmail.com>
2025-07-08 15:47:34 +0300

    Работа по тихоньку идет, сделал приглашение и добавил команду help

--- DonNixShell.c
+++ DonNixShell.c
@@ -8,28 +8,25 @@ int main() {
 
   printf("Привет, это DonNix\n");
 
-  printf("Перед началом работы с терминалом введи свое имя:");
+  printf("Перед началом работы с терминалом введи свое имя: ");
   scanf("%s", name);
   
   while (true) {
+    printf("DonnixShell ");
     scanf("%s", command);
     if (strcmp(command, "exit") == 0) {
       printf("Работа терминала завершена");
       break;
-    }
-    if (strcmp(command, "write")== 0) {
+    } else if (strcmp(command, "write")== 0) {
       scanf("%s", argument);
       printf("%s\n", argument);
 
-    } else {
-      printf("Ошибка, команда не найдена\n");
-    }
-    if (strcmp(command, "calculator") == 0) {
+    } else if (strcmp(command, "calculator") == 0) {
       int a,b;
       printf("Введите два числа: ");
       scanf("%d %d", &a, &b);
       char c;
-      printf("Введите знак: (+,-,*,/");
+      printf("Введите знак: (+,-,*,/) ");
       scanf(" %c", &c);
 
       if (c == '+') {
@@ -44,9 +41,21 @@ int main() {
       if (c ==  '/') {
         printf("Деление: %d\n", a / b);
       }
-    }
-    if (strcmp(command, "User") == 0) {
-      printf("%s\n",name);
+    } else if (strcmp(command, "user") == 0) {
+      printf("Вы авторизовались под пользователем: %s\n",name);
+    }else if (strcmp(command, "help") == 0) {
+      scanf("%s", argument);
+      if (strcmp(argument, "write") == 0) {
+        printf("Команда для вывода текста в консоль.\n");
+      }
+      if (strcmp(argument, "exit") == 0) {
+        printf("Комнда для завершения работы с терминалом.\n");
+      }
+      if (strcmp(argument, "calculator") == 0) {
+        printf("Простой калькулятор может сложить, разделить, умножить два числа.");
+      }
+    } else {
+      printf("Ошибка, такой команды нет\n");
     }
   }
   return 0;
